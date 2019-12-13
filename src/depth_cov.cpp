991613856dee08940f2291c8572ccf3d13ee1412
He <hxzhang1@ualr.edu>
2019-12-12 20:10:31 -0500

    make it work

--- src/depth_cov.cpp
+++ src/depth_cov.cpp
@@ -27,9 +27,27 @@
 using namespace std; 
 using namespace cv; 
 
-string base_dir(""); 
+struct loc{
+	int r,c; 
+	vector<double> dis; 
+};
+
+// range of interest 
+int lc = 100; 
+int rc = 550;
+int ur = 80; 
+int lr = 440;
+
+vector<vector<double>> v_dpts;
+
+string out_img("output.png"); 
+
+void init(); 
 
 void processBagfile(string bagfile); 
+
+void accumulate_data(const cv::Mat& dpt); 
+void compute_statics(cv::Mat& G);
 
 int main(int argc, char* argv[])
 {
@@ -37,11 +55,14 @@ int main(int argc, char* argv[])
   
   ros::NodeHandle nh; 
 
-  ROS_INFO("./depth_cov [bagfile]");
+  ROS_INFO("./depth_cov [bagfile] [*.png]");
 
   string bagfile = "";
   if(argc >= 2) 
     bagfile = argv[1]; 
+
+  if(argc >=3)
+  	out_img = argv[2]; 
 
   processBagfile(bagfile); 
 
@@ -63,6 +84,13 @@ void processBagfile(string bagfile)
 
   int cnt = 0; 
 
+  init();
+
+  // covert 16uc1 to 8uc1
+  // dis = u16_d * 0.001
+  // u8_d = dis / 7. * 255
+  double scale = 0.001/7.*255; 
+
   BOOST_FOREACH(rosbag::MessageInstance const m, view)
   {
       if(m.getTopic() == dpt_tpc || ("/"+m.getTopic()) == dpt_tpc)
@@ -71,14 +99,117 @@ void processBagfile(string bagfile)
         sensor_msgs::ImageConstPtr simage = m.instantiate<sensor_msgs::Image>();
         cv_ptrD = cv_bridge::toCvShare(simage, sensor_msgs::image_encodings::TYPE_16UC1); 
         // imwrite(d_dpt + "/"+tt.str() +".png", cv_ptrD->image); 
-        imshow("dpt_file", cv_ptrD->image); 
+        accumulate_data(cv_ptrD->image); 
+
+        // Holds the colormap version of the image:
+        Mat cm_img0;
+        Mat psudo_grey; 
+        // covert to 8UC1 
+        convertScaleAbs(cv_ptrD->image, psudo_grey, scale); 
+        // Apply the colormap:
+        applyColorMap(psudo_grey, cm_img0, COLORMAP_JET);
+
+        // imshow("dpt_file", cv_ptrD->image); 
+        imshow("pysudo dpth", cm_img0);
         cout<<"depth_cov.cpp: show "<<++cnt<<" depth data!"<<endl;
         waitKey(20); 
       }
       if(!ros::ok())
       	break; 
-     
   }
+  if(ros::ok()){
+  	cv::Mat G; 
+  	compute_statics(G); 
+  	Mat cm_img0;
+  	applyColorMap(G, cm_img0, COLORMAP_JET);
+  	imwrite(out_img, cm_img0); 
+  	imshow("statistic pysudo dpth", cm_img0);
+    waitKey(0); 
+  }
+   
+
   return ; 
 }
 
+
+void compute_statics(cv::Mat& G){
+
+	int row = lr - ur + 1; 
+	int col = rc - lc + 1;
+	G = cv::Mat(row, col, CV_8UC1);  
+	int i=-1; 
+	int N = row*col; 
+	vector<double> v_std(N, 0); 
+	double max_std = 0; 
+
+	double MAX_STD = 2.2; // so far 
+
+	for(int r=0; r<row; r++)
+	for(int c=0; c<col; c++)
+	{
+		++i;
+		// compte mean and std 
+		vector<double>& v = v_dpts[i];
+		if(v.size() <= 50){
+			continue; 
+		}
+		double sum = std::accumulate(std::begin(v), std::end(v), 0.0);
+		double m =  sum / v.size();
+
+		double accum = 0.0;
+		std::for_each (std::begin(v), std::end(v), [&](const double d) {
+		    accum += (d - m) * (d - m);
+		});
+
+		double stdev = sqrt(accum / (v.size()-1));
+		v_std[i] = stdev;
+		if(stdev > max_std)
+			max_std = stdev;  
+	}
+
+	if(max_std <= 0) {
+		cerr <<"what? max_std: "<<max_std<<endl; 
+		return ; 
+	}else{
+		cout<<" max_std: "<<max_std<<endl; 
+	}
+
+	i = -1; 
+	double ratio; 
+	for(int r=0; r<row; r++)
+	for(int c=0; c<col; c++)
+	{
+		++i;
+		ratio = v_std[i]/MAX_STD; 
+		if(ratio > 1.) ratio = 1.;
+		G.at<unsigned char>(r,c) = (unsigned char)(ratio*255);
+	}
+	return ; 
+}
+
+void accumulate_data(const cv::Mat& dpt){
+	
+	double dis; 
+
+	int inx = -1;
+	// cout<<" v_dpts.size(): "<<v_dpts.size()<<endl;  
+	for(int r=ur; r<=lr; r++)
+	for(int c=lc; c<=rc; c++)
+	{
+		dis = dpt.at<unsigned short>(r,c)*0.001; 
+		++inx; 
+		if(dis >0.5){
+			// cout<<"inx: "<<inx<<endl;
+			v_dpts[inx].push_back(dis); 
+		}
+	}
+}
+
+void init(){
+
+	for(int r=ur; r<=lr; r++)
+	for(int c=lc; c<=rc; c++)
+	{
+		v_dpts.push_back(vector<double>()); 
+	}
+}
