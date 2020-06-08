fa5f44f0517372a5240efad6971bf83fc53e6108
He Zhang <hxzhang1@ualr.edu>
2020-06-07 23:03:56 -0400

    add depth_distribute to compute std using MC for each depth

--- src/depth_distribute.cpp
+++ src/depth_distribute.cpp
@@ -1,8 +1,7 @@
 /*
-	
-	Dec. 12, 2019, He Zhang, hzhang8@vcu.edu 
+	June 7, 2020, He Zhang, hzhang8@vcu.edu 
 
-	analyze depth's data covariance with different  
+	Monte Carlo sampling to compute depth/inverse-depth standard deviation for each pixel
 
 */
 
@@ -23,9 +22,12 @@
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 #include <boost/foreach.hpp>
+#include "get_data.hpp"
 
 using namespace std; 
 using namespace cv; 
+
+#define SQ(x) (((x)*(x)))
 
 struct loc{
 	int r,c; 
@@ -47,55 +49,30 @@ int lr = 440;
 
 vector<vector<double>> v_dpts; // store repeated depth values 
 vector<MU> v_dpt_mean; // store mean depth value 
-vector<bool> v_dpt_valid; // decide whether this point's depth value is valid 
 
-double g_approximate_std = 0.02; 
-bool g_inverse_depth = false; 
-string out_img("output"); 
-string mu_img("");
-string pre_fix("");
+string g_mt_file("inv_monte_carlo.png"); 
+string gmm_img("inv_gmm.png");
+string ori_img("inv_ori.png");
 
 void init(); 
-
 void processBagfile(string bagfile); 
 
 void accumulate_data(const cv::Mat& dpt); // store depth data  
-void find_valid(); // find valid points
 void compute_statics(cv::Mat& G, cv::Mat&);
+cv::Mat covert_to_color(cv::Mat& );
 
 int main(int argc, char* argv[])
 {
-  ros::init(argc, argv, "depth_cov");
-  
+  ros::init(argc, argv, "depth_distribute");
   ros::NodeHandle nh; 
 
-  ROS_INFO("./depth_cov [bagfile] [output_image_name] [appro_std] [inv_cov]");
+  ROS_INFO("./depth_distribute [bagfile] [output_std.exr] [output_std.png]");
 
   string bagfile = "";
   if(argc >= 2) 
     bagfile = argv[1]; 
+	
 
-  if(argc >=3)
-  	out_img = argv[2]; 
-
-  if(argc >=4)
-  	g_approximate_std = atof(argv[3]); 
-
-  if(argc >=5){
-  	cout<<"argv[4]: "<<argv[4]<<endl; 
-  	g_inverse_depth = (string(argv[4])==string("true"));
-  }
-
-  if(g_inverse_depth){
-  	ROS_WARN("depth_cov.cpp: now play with inverse depth !!!!");
-  	pre_fix = "inv_";
-  }else{
-  	ROS_WARN("depth_cov.cpp: in depth !");
-  }
-
-  cout<<"g_approximate_std: "<<g_approximate_std<<endl;
-  mu_img = pre_fix + "mu_" + out_img + ".png"; 
-  out_img = pre_fix + out_img+".exr";
   processBagfile(bagfile); 
 
   return 0; 
@@ -105,7 +82,9 @@ void processBagfile(string bagfile)
 {
   std::vector<std::string> topics;
   string dpt_tpc = "/cam0/depth"; 
+  string rgb_tpc = "/cam0/color"; 
   topics.push_back(dpt_tpc); 
+  topics.push_back(rgb_tpc); 
   
   rosbag::Bag bag; 
   bag.open(bagfile, rosbag::bagmode::Read); 
@@ -118,43 +97,57 @@ void processBagfile(string bagfile)
 
   init();
 
-  BOOST_FOREACH(rosbag::MessageInstance const m, view)
-  {
-      if(m.getTopic() == dpt_tpc || ("/"+m.getTopic()) == dpt_tpc)
-      {
+  static bool first = true; 
+  cv::Mat prd; 
+  cv::Mat gmm; 
+  BOOST_FOREACH(rosbag::MessageInstance const m, view){
+	 
+	 if(m.getTopic() == dpt_tpc || ("/"+m.getTopic()) == dpt_tpc){
         // receive a dpt image
         sensor_msgs::ImageConstPtr simage = m.instantiate<sensor_msgs::Image>();
         cv_ptrD = cv_bridge::toCvShare(simage, sensor_msgs::image_encodings::TYPE_16UC1); 
-        // imwrite(d_dpt + "/"+tt.str() +".png", cv_ptrD->image); 
+
+        if(first){
+
+        	first = false; 
+        	usleep(1000); 
+        	continue; 
+        }
         accumulate_data(cv_ptrD->image); 
-        cout<<"depth_cov.cpp: show "<<++cnt<<" depth data!"<<endl;
+        cout<<"depth_distribute.cpp: show "<<++cnt<<" depth data!"<<endl;
+        // waitKey(20); 
       }
       if(!ros::ok())
       	break; 
   }
 
-  // remove invalid point
-  find_valid(); 
-
   if(ros::ok()){
   	cv::Mat G, MU; 
   	compute_statics(G, MU); 
-  	Mat cm_img0;
-  	// applyColorMap(G, cm_img0, COLORMAP_JET);
-  	imwrite(out_img, G); 
+
+  	// double rmse_prd = rmse_diff(G, prd); 
+  	// double rmse_gmm = rmse_diff(G, gmm);
+
+  	// ROS_INFO("gmm_depth.cpp: rmse_prd: %lf", rmse_prd);
+  	// ROS_INFO("gmm_depth.cpp: rmse_gmm: %lf", rmse_gmm);
+
+  	Mat mt_img = covert_to_color(G); 
+  	imwrite("output_std.exr", G); 
+  	imwrite("output_std.png", mt_img); 
+  	// imwrite(g_mt_file.c_str(), mt_img); 
+
+  	// Mat cm_img0;
   	// applyColorMap(MU, cm_img0, COLORMAP_JET); 
-  	// imshow("std pysudo dpth", cm_img0);
-  	imwrite(mu_img, MU);
-    // waitKey(1000); 
+  	imshow("mean_depth: ", MU);
+    waitKey(2000); 
   }
    
 
   return ; 
 }
 
-
-void compute_statics(cv::Mat& G, cv::Mat& MU){
-
+void compute_statics(cv::Mat& G, cv::Mat& MU)
+{
 	int row = lr - ur + 1; 
 	int col = rc - lc + 1;
 	// G = cv::Mat(row, col, CV_16UC1);  
@@ -167,8 +160,9 @@ void compute_statics(cv::Mat& G, cv::Mat& MU){
 	double max_std = 0; 
 
 	double MAX_STD = 0.2; //2.2; // so far 
-	double MAX_DIS = 7.5; 
-	double MAX_INV_DIS = 1/0.6;
+	double MAX_DIS = 7.5;
+	double MAX_INV_DIS = 1./0.5; 
+	double MAX_INV_STD = 0.002;
 	int MAX_N = 65535; 
 
 	for(int r=0; r<row; r++)
@@ -180,9 +174,6 @@ void compute_statics(cv::Mat& G, cv::Mat& MU){
 		if(v.size() <= 100){
 			continue; 
 		}
-		if(!v_dpt_valid[i])
-			continue;
-
 		double sum = std::accumulate(std::begin(v), std::end(v), 0.0);
 		double m =  sum / v.size();
 
@@ -211,18 +202,12 @@ void compute_statics(cv::Mat& G, cv::Mat& MU){
 	for(int c=0; c<col; c++)
 	{
 		++i;
-		ratio = v_std[i]/MAX_STD; 
+		ratio = v_std[i]/MAX_INV_STD; 
 		if(ratio > 1.) ratio = 1.;
 		// G.at<unsigned char>(r,c) = (unsigned char)(ratio*255);
 		// G.at<unsigned short>(r,c) = (unsigned short)(ratio*MAX_N);
 		G.at<float>(r,c) = v_std[i]; 
-		// ratio = v_mean[i]/MAX_DIS; 
-
-		if(g_inverse_depth)
-			ratio = v_mean[i]/MAX_INV_DIS; 
-		else
-			ratio = v_mean[i]/MAX_DIS; 
-
+		ratio = v_mean[i]/MAX_INV_DIS; 
 		if(ratio > 1.) ratio = 1;
 		// MU.at<unsigned char>(r,c) = (unsigned char)(ratio*MAX_N);
 		MU.at<unsigned short>(r,c) = (unsigned short)(ratio*MAX_N);
@@ -230,57 +215,8 @@ void compute_statics(cv::Mat& G, cv::Mat& MU){
 	return ; 
 }
 
-void find_valid(){ // find valid points
-
-	double dis; 
-
-	int inx = -1;
-	int ws = 3; 
-	int COL = rc-lc+1; 
-	int thre_num = (int)((ws*2+1)*(ws*2+1)/3);
-	double thre_dis = 10*g_approximate_std; 
-	// cout<<" v_dpts.size(): "<<v_dpts.size()<<endl;  
-	for(int r=ur; r<=lr; r++)
-	for(int c=lc; c<=rc; c++)
-	{
-		++inx;
-		int val_num = 0; 
-		int inv_num = 0; 
-		double max_dis = -1; 
-		double min_dis = 10000; 
-		// work in the local window 
-		for(int rr=r-ws; rr<=r+ws; ++rr){
-		for(int cc=c-ws; cc<=c+ws; ++cc){
-			if(rr<ur || rr>lr || cc<lc || cc>rc)
-				continue; 
-
-			int cur_inx = (rr-ur)*COL + (cc-lc+1); 
-			dis = v_dpt_mean[cur_inx].mean; 
-
-			if(!g_inverse_depth && dis <= 0.5) {
-				inv_num++; 
-				continue; 
-			}else if(g_inverse_depth && dis >= (1./0.5)){
-				inv_num++;
-				continue;
-			}
-			val_num++;
-			if(max_dis < dis) max_dis = dis; 
-			if(min_dis > dis) min_dis = dis;
-
-		}
-		}
-
-		if(inv_num >= thre_num) 
-			v_dpt_valid[inx] = false; 
-		else if(max_dis - min_dis > thre_dis){
-			v_dpt_valid[inx] = false;
-		}
-	}
-
-}
-void accumulate_data(const cv::Mat& dpt){
-	
+void accumulate_data(const cv::Mat& dpt)
+{	
 	double dis; 
 
 	int inx = -1;
@@ -291,24 +227,64 @@ void accumulate_data(const cv::Mat& dpt){
 		dis = dpt.at<unsigned short>(r,c)*0.001; 
 		++inx; 
 		if(dis >0.5){
-
-			if(g_inverse_depth)
-				dis = 1./dis; 
-
 			// cout<<"inx: "<<inx<<endl;
+			dis = 1./dis; 
 			v_dpts[inx].push_back(dis); 
 			v_dpt_mean[inx].mean = (dis+v_dpt_mean[inx].sum())/(++v_dpt_mean[inx].num);
 		}
 	}
 }
 
-void init(){
+double rmse_diff(cv::Mat& d1, cv::Mat& d2)
+{
+	unsigned int cnt = 0; 
+	double sum_se = 0; 
 
+	for(int r=0; r<d1.rows; r++)
+	for(int c=0; c<d1.cols; c++){
+
+		double std1 = d1.at<float>(r,c); 
+		double std2 = d2.at<float>(r,c); 
+
+		if(std1 == 0 || std2 == 0)
+			continue; 
+		sum_se += SQ(std1 - std2); 
+		++cnt;  
+	}
+	double rmse = 0; 
+	if(cnt > 0) rmse = sqrt(sum_se/cnt); 
+	return rmse; 
+}
+
+
+void init()
+{
 	for(int r=ur; r<=lr; r++)
 	for(int c=lc; c<=rc; c++)
 	{
 		v_dpts.push_back(vector<double>()); 
 		v_dpt_mean.push_back(MU());
-		v_dpt_valid.push_back(true);
 	}
+}
+
+cv::Mat covert_to_color(cv::Mat& d)
+{
+	cv::Mat color= cv::Mat(d.rows, d.cols, CV_8UC1, Scalar(0)); 
+
+	double MAX_STD = 0.055; 
+	double MAX_INV_STD = 0.004; 
+
+	for(int r=0; r<d.rows; r++)
+	for(int c=0; c<d.cols; c++){
+		double std = d.at<float>(r,c); 
+		double ratio = std / MAX_INV_STD;
+		ratio = ratio>1? 1.:ratio;
+		color.at<unsigned char>(r,c) = (unsigned char)( ratio * 255);
+	}
+
+	Mat cm_img0;
+  	applyColorMap(color, cm_img0, COLORMAP_HOT);
+  	// imwrite(out_img, G); 
+
+	return cm_img0;
 }
